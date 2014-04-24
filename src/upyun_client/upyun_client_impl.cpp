a9eb8cf7665430e6fa8f5d3dae7a4f5d71b75d5b
stlf <stlf@live.cn>
2014-04-24 15:41:17 +0800

    1.

---
+++ src/upyun_client/upyun_client_impl.cpp
@@ -0,0 +1,6 @@
+#include "upyun_client_impl.h"
+
+upyun_client_impl::upyun_client_impl(QObject *parent) :
+    QObject(parent)
+{
+}
---
+++ src/upyun_client/upyun_client_impl.h
@@ -0,0 +1,18 @@
+#ifndef UPYUN_CLIENT_IMPL_H
+#define UPYUN_CLIENT_IMPL_H
+
+#include <QObject>
+
+class upyun_client_impl : public QObject
+{
+    Q_OBJECT
+public:
+    explicit upyun_client_impl(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // UPYUN_CLIENT_IMPL_H
