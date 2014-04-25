c6e5b4f9c9ad20ffb3843913389f660a2199cb73
stlf <stlf@live.cn>
2014-04-25 16:46:51 +0800

    implement uploadFile interface, need to finish...

--- src/upyun_client/upyun_client_impl.cpp
+++ src/upyun_client/upyun_client_impl.cpp
@@ -1,7 +1,92 @@
+#include <time.h>
+#include <QUrlQuery>
+#include <QCryptographicHash>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QDebug>
+#include <QFile>
+#include <string>
 #include "upyun_client_impl.h"
 
-upyun_client_impl::upyun_client_impl(const QString &usr, const QString &pass, QObject *parent) :
-    QObject(parent)
+const static QString upyun_api_host = "https://v0.api.upyun.com";
+
+std::string rfc1123_datetime( time_t time )
+{
+    struct tm * timeinfo;
+    char buffer [80];
+
+    timeinfo = gmtime ( &time );
+    strftime (buffer,80,"%a, %d %b %Y %H:%M:%S GMT",timeinfo);
+
+    return buffer;
+}
+
+QString get_sign(const QString &method, const QString &path_url, const QString &now_time,
+                        const QString &content_len, const QString &pwd)
+{
+    QString linkstr = method + "&" + path_url + "&" +
+            now_time + "&" + content_len;
+
+    QString pwd_hash = QCryptographicHash::hash(pwd.toUtf8(), QCryptographicHash::Md5).toHex();
+
+    linkstr += "&" + pwd_hash;
+
+    return QCryptographicHash::hash(linkstr.toUtf8(), QCryptographicHash::Md5).toHex();
+}
+
+QString get_auth_string(const QString &usr, const QString &sign)
+{
+    QString auth =  "UpYun " + usr +":";
+    auth.append(sign);
+    return auth;
+}
+
+upyun_client_impl::upyun_client_impl(const QString &usr, const QString &pass, const QString &bucket,
+                                     QObject *parent) :
+    _usr(usr), _pwd(pass), _bucket(bucket), QObject(parent)
 {
 
+}
+
+QNetworkReply* upyun_client_impl::uploadFile(const QString &local_path,
+                                             const QString &remote_path)
+{
+    // read file
+    QFile file(local_path);
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        error("can not open or read file:" + local_path);
+    }
+
+    const QByteArray &file_data = file.readAll();
+    QString content_len = QString::number(file_data.length());
+    QString path_url =  "/" + _bucket + "/" + remote_path;
+    QString now_time =  rfc1123_datetime(time(NULL)).c_str();
+
+    QString auth = get_auth_string(_usr,
+                                   get_sign("PUT", path_url, now_time, content_len, _pwd));
+
+    QUrl url = upyun_api_host + path_url;
+
+    QNetworkRequest request(url);
+    request.setRawHeader("Authorization", auth.toLatin1());
+    request.setRawHeader("Content-Length", content_len.toLatin1());
+    request.setRawHeader("Date", now_time.toLatin1());
+    request.setRawHeader("mkdir","true");
+
+    QNetworkReply *reply = _qnam.put(request, file_data);
+
+    QObject::connect(reply, SIGNAL(finished()), this, SIGNAL(finished()));
+
+    // QObject::connect(reply, SIGNAL(error(QNetworkReply::NetworkError))()), this, SIGNAL(finished()));
+    // TODO: connect error handle
+
+    return reply;
+
+
+}
+
+QNetworkReply* upyun_client_impl::downloadFile(const QString &path)
+{
+    return NULL;
 }
--- src/upyun_client/upyun_client_impl.h
+++ src/upyun_client/upyun_client_impl.h
@@ -2,16 +2,34 @@
 #define UPYUN_CLIENT_IMPL_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
 
+class QNetworkReply;
 class upyun_client_impl : public QObject
 {
     Q_OBJECT
 public:
-    explicit upyun_client_impl(const QString &usr, const QString &pass, QObject *parent = 0);
+    explicit upyun_client_impl(const QString &usr, const QString &pass, const QString &bucket,
+                               QObject *parent = 0);
 
 signals:
+    void uploadProgress(qint64 bytesSent, qint64 bytesTotal);
+    void downloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+
+    void error(const QString &e);
+    void finished();
 
 public slots:
+    QNetworkReply *uploadFile(const QString &local_path, const QString &remote_path);
+    QNetworkReply *downloadFile(const QString &remote_path);
+
+private:
+    QString _usr;
+    QString _pwd;
+    QString _bucket;
+
+    QNetworkAccessManager _qnam;
+    QNetworkReply *_reply;
 
 };
 
